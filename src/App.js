1819bdef12efb4a5f7e81470d0877f3869914eab
Nataly Kozyreva <eve30@yandex.ru>
2024-02-05 12:02:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { Chart } from "react-google-charts";
 import './App.css';
 
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 9],
+  ["Eat", 3],
+  ["Commute", 3],
+  ["Watch TV", 4],
+  ["Sleep", 8],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
     </div>
   );
 }
